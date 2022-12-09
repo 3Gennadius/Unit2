ac392939c2c8e3a99edefec2a563219d5248dbcf
3Gennadius <115481973+3Gennadius@users.noreply.github.com>
2022-12-09 01:00:11 +0000

    update

--- Unit2_B00139774/Library/Bee/artifacts/emscripten/94vj1/wrapper_emar.bat
+++
@@ -1,4 +0,0 @@
-
-@echo off
-"D:\Unity\Unity\2021.3.10f1\Editor\Data\PlaybackEngines\WebGLSupport\BuildTools\Emscripten\emscripten\emar.bat" %* < nul
-exit %ERRORLEVEL%
--- Unit2_B00139774/Library/Bee/artifacts/emscripten/ot4lb/wrapper_emcc.bat
+++
@@ -1,4 +0,0 @@
-
-@echo off
-"D:\Unity\Unity\2021.3.10f1\Editor\Data\PlaybackEngines\WebGLSupport\BuildTools\Emscripten\emscripten\emcc.bat" %* < nul
-exit %ERRORLEVEL%
